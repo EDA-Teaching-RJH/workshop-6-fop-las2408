92df1d48f0f89dfe0c2b2f5ef8cce3b0b4aa30a7
las2408 <las1@kent.ac.uk>
2026-02-09 12:12:26 +0000

    3.1

--- part_3.py
+++ part_3.py
@@ -1,1 +1,15 @@
+#3.1
+while True:
+        try:
+               motor_speed = int(input("Enter Motor Speed: "))
+        except ValueError:
+            print("Error: Corrupted Signal. Maintaining current speed.")
+            continue
+        
+        else:
+              print(f"Speed set to {motor_speed}")
+              break
+
+            
+    
 

78ed49e49956465be2c63988c4600c485ff3661e
las2408 <las1@kent.ac.uk>
2026-02-09 11:30:37 +0000

    1.4

--- part_1.py
+++ part_1.py
@@ -1,19 +1,25 @@
-#1.3
+#1.4
 sample_bay = [ "Basalt", "Silica", "Iron", "Dust" ]
 new_findings = []
-
-i = 0
-while i < 3:
-    rock = str(input("Please input the rock type: "))
-    new_findings.append(rock)
-    i = i + 1
-    continue
-else:
-    print(new_findings)
-
-#for sample in sample_bay:
-    #print(f"Transmitting data for: {sample}")
 
 #print(sample_bay[0])
 #print(sample_bay[-1])
 #print(len(sample_bay))
+
+#for sample in sample_bay:
+    #print(f"Transmitting data for: {sample}")
+
+#i = 0
+#while i < 3:
+    #rock = str(input("Please input the rock type: "))
+    #new_findings.append(rock)
+    #i = i + 1
+    #continue
+#else:
+    #print(new_findings)
+
+if "Dust" in sample_bay:
+    sample_bay.remove("Dust")
+    print(sample_bay)
+else:
+    print(sample_bay)

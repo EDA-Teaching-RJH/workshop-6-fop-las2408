f3e4523718cdda388e18d3f439a74ac7d818c509
las2408 <las1@kent.ac.uk>
2026-02-09 12:15:53 +0000

    3.2

--- part_3.py
+++ part_3.py
@@ -1,14 +1,29 @@
-#3.1
-while True:
+#3.2
+
+def get_coordinate():
+    while True:
         try:
-               motor_speed = int(input("Enter Motor Speed: "))
+            coord = int(input("Please enter an x coordinate: "))
         except ValueError:
-            print("Error: Corrupted Signal. Maintaining current speed.")
+            print("Invalid coordinate")
             continue
-        
         else:
-              print(f"Speed set to {motor_speed}")
-              break
+            break
+        return coord
+    
+    print(coord)
+
+get_coordinate()
+#while True:
+        #try:
+               #motor_speed = int(input("Enter Motor Speed: "))
+        #except ValueError:
+            #print("Error: Corrupted Signal. Maintaining current speed.")
+            #continue
+        
+        #else:
+              #print(f"Speed set to {motor_speed}")
+              #break
 
             
     

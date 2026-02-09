308f1f0b9d3166404996e668472059dcf54e074c
las2408 <las1@kent.ac.uk>
2026-02-09 12:31:13 +0000

    4.1

--- part_4.py
+++ part_4.py
@@ -1,1 +1,19 @@
+#4.1
+def sensor():
+    travel_log = []
+    while True:
+        try:
+            angle = int(input("Please enter sensor reading (Slope Angle): "))
+        except ValueError:
+            print("Sensor Glitch")
+            continue
+        else:
+            if angle > 45:
+                print("CRITICAL TILT! HALTING.")
+                break
+            else:
+                travel_log.append(angle)
+                print("Path Stable. Moving Forward")
+                continue
 
+sensor()
